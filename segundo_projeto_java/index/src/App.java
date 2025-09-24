a93944377fef495aae2452f3032cbccbfe842d41
LanyIsabella <lanyisabella123@gmail.com>
2025-09-23 21:48:30 -0300

    Adição de veículo

--- segundo_projeto_java/index/src/App.java
+++ segundo_projeto_java/index/src/App.java
@@ -1,18 +1,32 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Veiculo carro = new Veiculo();
+        Veiculo carro1 = new Veiculo();
 
-        carro.setId(1);
-        carro.setPlaca("ABC-1234");
-        carro.setNome("Fusca");
-        carro.setModelo(1967);
-        carro.setAno(1967);
-        System.out.println("ID: " + carro.getId());
-        System.out.println("Placa: " + carro.getPlaca());
-        System.out.println("Nome: " + carro.getNome());
-        System.out.println("Modelo: " + carro.getModelo());
-        System.out.println("Ano: " + carro.getAno());
+        carro1.setId(1);
+        carro1.setPlaca("ABC-1234");
+        carro1.setNome("Fusca");
+        carro1.setModelo(1967);
+        carro1.setAno(1967);
+        System.out.println("ID: " + carro1.getId());
+        System.out.println("Placa: " + carro1.getPlaca());
+        System.out.println("Nome: " + carro1.getNome());
+        System.out.println("Modelo: " + carro1.getModelo());
+        System.out.println("Ano: " + carro1.getAno());
         System.out.println("---------------------");
+
+        Veiculo carro2 = new Veiculo();
+        carro2.setId(2);
+        carro2.setPlaca("DEF-5678");
+        carro2.setNome("Brasília");
+        carro2.setModelo(1973);
+        carro2.setAno(1973);
+        System.out.println("ID: " + carro2.getId());
+        System.out.println("Placa: " + carro2.getPlaca());
+        System.out.println("Nome: " + carro2.getNome());
+        System.out.println("Modelo: " + carro2.getModelo());
+        System.out.println("Ano: " + carro2.getAno());
+        System.out.println("---------------------");
+
 
         Fabricante volkswagen = new Fabricante();
         volkswagen.setId(1);
